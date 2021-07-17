9acdd208a3048080cf001509207cc96fb0b909a6
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-07-17 11:35:26 +0100

    update provider

--- src/app/update-provider/update-provider.component.ts
+++ src/app/update-provider/update-provider.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProviderService } from './../services/provider.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-update-provider',
   templateUrl: './update-provider.component.html',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProviderComponent implements OnInit {
 
-  constructor() { }
+  public id;
+  public providerToUpdate;
+  public name;
+  public email;
+  public adress;
 
-  ngOnInit(): void {
+  constructor(private service: ProviderService, private router: Router, private activatedroute: ActivatedRoute) { }
+
+
+  ngOnInit() {
+    this.activatedroute.paramMap.subscribe(
+      params => {
+        this.id = params.get('id');  // récupération de lid
+
+        this.providerToUpdate = this.service.getProvider(this.id).subscribe(
+          response => {
+            //console.log(response);
+            this.name = response["name"];
+            this.email = response["email"];
+            this.adress = response["address"];
+          }
+        );
+
+      }
+    );
+  }
+
+  updateProvider() {
+    this.providerToUpdate = {
+      'name': this.name,
+      'email': this.email,
+      'address': this.adress,
+      'id': this.id
+    }
+    this.service.updateProvider(this.providerToUpdate).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['listProvider']);
+      }
+    );
+
   }
 
 }
